7f98b5f2771915886695302f5e908658c392c4b6
John Zhang <johntostring@gmail.com>
2017-02-26 23:30:07 +0800

    goods-restful: post to create a category

--- goods-module/goods-restful-api/src/main/java/com/millinch/mall/goods/web/CategoryApi.java
+++ goods-module/goods-restful-api/src/main/java/com/millinch/mall/goods/web/CategoryApi.java
@@ -1,11 +1,11 @@
 package com.millinch.mall.goods.web;
 
+import com.millinch.core.domain.ResponseModel;
 import com.millinch.mall.goods.entity.Category;
 import com.millinch.mall.goods.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +24,17 @@ public class CategoryApi {
     @GetMapping
     public List<Category> getCategoryTree() {
         return categoryService.getCategoryTree();
+    }
+
+    @PostMapping
+    public ResponseEntity createCategory(@RequestBody Category category) {
+        boolean insert = categoryService.insert(category);
+        ResponseModel.Builder builder = ResponseModel.builder().ok(insert);
+        if (insert) {
+            builder.body(category).message("创建成功");
+        } else {
+            builder.message("创建失败");
+        }
+        return ResponseEntity.ok(insert);
     }
 }
