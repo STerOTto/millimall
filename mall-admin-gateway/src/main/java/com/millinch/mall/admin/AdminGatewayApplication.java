9f919365b80cbf4df8e49ba0d74c063c0ab88bf9
John Zhang <johntostring@gmail.com>
2017-03-02 21:56:57 +0800

    admin-gateway: pure admin api gateway without oauth2 integration

--- mall-admin-gateway/src/main/java/com/millinch/mall/admin/AdminGatewayApplication.java
+++ mall-admin-gateway/src/main/java/com/millinch/mall/admin/AdminGatewayApplication.java
@@ -2,37 +2,52 @@ package com.millinch.mall.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * This guy is lazy, nothing left.
  *
  * @author John Zhang
  */
+@Controller
 @EnableZuulProxy
-@EnableOAuth2Sso
 @EnableDiscoveryClient
 @SpringBootApplication
-public class AdminGatewayApplication extends WebSecurityConfigurerAdapter {
+public class AdminGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminGatewayApplication.class, args);
     }
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-            .logout().and()
-            .authorizeRequests()
-            .antMatchers("/index.html", "/", "/login").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .csrf()
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    @GetMapping
+    @ResponseBody
+    public String index() {
+        return "AdminGatewayApplication is running";
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
