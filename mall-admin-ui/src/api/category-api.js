ea66e52ef1212b10a84663cf04a7c6cd5639ce78
John Zhang <johntostring@gmail.com>
2017-03-06 16:49:56 +0800

    admin-ui: do not need JSON.stringify(), it will make axios `Content-Type` header failed

--- mall-admin-ui/src/api/category-api.js
+++ mall-admin-ui/src/api/category-api.js
@@ -3,7 +3,7 @@ import axios from '../api/http'
 let adminURL = 'http://localhost:9898'
 
 export const createCategory = (params) => {
-  return axios.post(`${adminURL}/goods/categories`, JSON.stringify(params))
+  return axios.post(`${adminURL}/goods/categories`, params)
 }
 
 export const getCategoryList = (params) => {
