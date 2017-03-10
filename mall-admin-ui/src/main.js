d877225a8667dcbd371268293d3cfb85203f6a56
John Zhang <johntostring@gmail.com>
2017-03-10 23:55:55 +0800

    admin-ui: commit auth state correctly

--- mall-admin-ui/src/main.js
+++ mall-admin-ui/src/main.js
@@ -8,11 +8,11 @@ import 'element-ui/lib/theme-default/index.css'
 import router from './routes'
 import axios from './api/http'
 import Breadcrumb from './components/Breadcrumb'
+import * as types from './store/mutation-types'
 
 Vue.use(ElementUI)
 Vue.prototype.axios = axios
 Vue.component('admin-breadcrumb', Breadcrumb)
-import * as types from './store/mutation-types'
 
 /* eslint-disable no-new */
 new Vue({
@@ -27,5 +27,25 @@ new Vue({
 let authInfo = window.sessionStorage.getItem('MALL_ADMIN_AUTH')
 if (authInfo) {
   authInfo = JSON.parse(authInfo)
+  console.log('refresh commit authInfo', authInfo)
   store.commit(types.LOGIN, {authInfo})
+  console.log('after commit authInfo', store.state.auth.authInfo)
 }
+
+router.beforeEach((to, from, next) => {
+  // if (to.matched.some(r => r.meta.requireAuth)) {
+  //
+  // } else {
+  //   next();
+  // }
+  console.log(store.state.auth.authInfo)
+  if (to.path === '/login' || store.state.auth.authInfo.access_token) {
+    next()
+  } else {
+    console.log('require login', store.state.auth.authInfo)
+    next({
+      path: '/login',
+      query: {redirect: to.fullPath}
+    })
+  }
+})
